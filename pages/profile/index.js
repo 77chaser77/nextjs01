27fad28e2a8a32d75b636b99c6af4335508d2f34
77chaser77 <eddy1206@naver.com>
2022-10-19 21:31:26 +0900

    dd

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 export default function ProfilePage() {
   return (
-    <div>/pages/profile/index.js</div>
-  )
+    <div>
+      <h1>정보보호학과 이창민</h1>
+      <img
+        name='ig'
+        src='Screenshot_20211125-2302473.png'
+        width='400'
+        height='200'
+        border='3'
+      ></img>
+    </div>
+  );
 }
